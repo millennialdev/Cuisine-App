04e669e7ccca5b7c50298eb2bc6188c6c6348d51
Spenser Wellen <spenserwellen@gmail.com>
2021-09-29 11:19:22 -0500

    added extra inputs to form.

--- src/App.js
+++ src/App.js
@@ -5,31 +5,36 @@ import Recipe from './components/Recipe';
 import Alert from './components/Alert';
 
 function App() {
-	const [query, setQuery] = useState('');
-	const [ingredients, setIngredients] = useState('yogurt');
-	const [exclusions, setExclusions] = useState('ginger');
+	const [cuisine, setCuisine] = useState('');
+	const [ingredients, setIngredients] = useState('');
+	const [exclusions, setExclusions] = useState('');
+
 	const [recipes, setRecipes] = useState([]);
 	const [alert, setAlert] = useState('');
 
 	const API_KEY = process.env.REACT_APP_API_KEY;
-	const url = `https://api.spoonacular.com/recipes/search?query=${query}&excludeIngredients=${exclusions}&includeIngredients=${ingredients}&apiKey=${API_KEY}`;
+	const url = `https://api.spoonacular.com/recipes/search?query=${cuisine}&excludeIngredients=${exclusions}&includeIngredients=${ingredients}&apiKey=${API_KEY}`;
 
 	const getData = async () => {
-		if (query !== '') {
+		if (cuisine !== '') {
 			const result = await Axios.get(url);
 
 			if (!result.products === []) {
 				return setAlert('No food with such name');
 			}
 			setRecipes(result.data.results);
-			setQuery('');
+			setCuisine('');
+			setIngredients('');
+			setExclusions('');
 			setAlert('');
 		} else {
 			setAlert('Please fill the form');
 		}
 	};
 
-	const onChange = (e) => setQuery(e.target.value);
+	const onCuisineChange = (e) => setCuisine(e.target.value);
+	const onIngredientsChange = (e) => setIngredients(e.target.value);
+	const onExclusionChange = (e) => setExclusions(e.target.value);
 
 	const onSubmit = (e) => {
 		e.preventDefault();
@@ -41,14 +46,32 @@ function App() {
 			<h1>Cuisine Helper {process.env.API_KEY}</h1>
 			<form onSubmit={onSubmit} className='search-form'>
 				{alert !== '' && <Alert alert={alert} />}
-				<input
-					type='text'
-					name='query'
-					onChange={onChange}
-					value={query}
-					autoComplete='off'
-					placeholder='Enter a cuisine'
-				/>
+				<div className='inputs-container'>
+					<input
+						type='text'
+						name='cuisine'
+						onChange={onCuisineChange}
+						value={cuisine}
+						autoComplete='off'
+						placeholder='Enter a cuisine'
+					/>
+					<input
+						type='text'
+						name='cuisine'
+						onChange={onIngredientsChange}
+						value={ingredients}
+						autoComplete='off'
+						placeholder='Enter ingredients (commas seperating)'
+					/>
+					<input
+						type='text'
+						name='cuisine'
+						onChange={onExclusionChange}
+						value={exclusions}
+						autoComplete='off'
+						placeholder='Enter exclusions with (commas seperating)'
+					/>
+				</div>
 				<input type='submit' value='Search' />
 			</form>
 			<div className='recipes'>
